c3bf851414cba18742daa741d674cb8feb698d0b
Joe Mastrianni <jmastrianni13@gmail.com>
2023-12-27 23:15:07 -0500

    done for now

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -40,10 +40,26 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        } else {
+            let mut parts: Vec<String> = s.split(",").map(|s| s.to_string()).collect(); 
+            if parts.len() < 2 {
+                return Person::default();
+            }
+            let name = parts.remove(0); 
+            if name.len() == 0 {
+                return Person::default();
+            } else {
+                let age = parts.remove(0).parse::<usize>();
+                match age {
+                    Ok(a) => Person { name, age: age.unwrap() },
+                    Err(a) => Person::default(),
+                }
+            }
+        }
     }
 }
 
